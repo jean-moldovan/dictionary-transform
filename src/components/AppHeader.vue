48e7c744bb7d69b47416abb83de9c21b3b5d2b3d
jean-moldovan <jean.moldovan@gmail.com>
2021-01-25 11:44:05 +0100

    cleanup after scaffolding

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <router-link to="/">
+      Dictionary Transformer
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
